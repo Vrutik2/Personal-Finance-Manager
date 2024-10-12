c3a6a37965c0d469a92c801c9e794cf87732366e
Vrutik Patel <vrutikpatel2022@gmail.com>
2024-10-12 13:50:22 -0400

    Initial Commit - Personal Finance Manager

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Personal Finance Manager',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Personal Finance Manager Home'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: const Text(
+          'Welcome to Personal Finance Manager!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
